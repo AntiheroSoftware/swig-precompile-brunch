f5d98660114bb6fde1a2ff7c20b88bac097a93af
lintbe <jfdusar@gmail.com>
2015-03-04 13:23:21 +0100

    Fix depedencies including includes et extend precompile fix

--- index.js
+++ index.js
@@ -15,19 +15,32 @@
         SwigCompiler.prototype.extension = 'html';
 
         SwigCompiler.prototype.getDependencies = function(data, path, callback) {
-            var match = data.match(/extends '([a-zA-Z\/]*)'/i);
             var dependencies = [];
+
+            // check for extends
+            var match = data.match(/extends '([a-zA-Z\/\.]*)'/i);
             if(match && match[1]) {
+                console.log("MATCH extends");
+                console.log(match);
                 dependencies.push("app/"+match[1]+".html");
             }
+
+            // check for includes
+            var re = /include '([a-zA-Z\/\.]*)'/ig;
+            while (match = re.exec(data)) {
+                console.log("MATCH include");
+                console.log(match);
+                dependencies.push("app/"+match[1]+".html");
+            }
+
             callback(null, dependencies);
         };
 
         SwigCompiler.prototype.compile = function(data, path, callback) {
-            var error, filename, result;
+            var error, result;
             try {
-                //var name = path.replace(/^app\//, '').split(".")[0];
-                return result = swig.compile(path, {});
+                result = swig.precompile(data, { filename: path});
+                return result = "module.exports = " + result.tpl.toString().replace(' anonymous', '');
             } catch (err) {
                 return error = err;
             } finally {
